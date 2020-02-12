ddf4e5401fad3841eb9ac3ac2937e47936b4d249
StoopDJ <amirakak@gmail.com>
2020-02-12 14:20:10 +0000

    loop

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -7,10 +7,12 @@ public class Loops extends PApplet
 	public void settings()
 	{
 		size(500, 500);
+	
 	}
 
 	public void setup() 
 	{
+		background(255);
 		
 	}
 
@@ -21,8 +23,42 @@ public class Loops extends PApplet
 	}	
 
 
+	/*
 	public void draw()
 	{	
-		background(0);		
-	}
+		background(0);
+		
+		stroke(255);
+
+		line(10, 10, 100, 10);
+		line(10, 20, 100, 20);
+		line(10, 30, 100, 30);
+		line(10, 40, 100, 40);
+
+		for(int y =0; y<=40; y +=10)
+		{
+			line(200, y, 300, y);
+		}
+
+		int yy=10;
+		while(yy <=40)
+		{
+			line(300, yy, 400, yy);
+			yy++;
+		}
+		*/
+		
+		public void draw() {
+			//int x = 0;
+			int x=0;
+			stroke(0);
+			// x is available! Since it is declared within the draw() block of code, it is available here. 
+			// Notice, however, that it is not available inside draw() above where it is declared. 
+			// Also, it is available inside the while block of code because while is inside of draw().
+			while (x < width) {
+			  stroke(0);
+			  line(x,0,x,height);
+			  x += 15;
+			} 
+		}	
 }
