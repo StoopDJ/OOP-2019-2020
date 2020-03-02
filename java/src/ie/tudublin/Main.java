5cf254133ed955fa6d7a26ba9fbc9eea4c90d196
StoopDJ <amirakak@gmail.com>
2020-03-02 12:07:44 +0000

    commit

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -24,13 +24,6 @@ public class Main
         final String[] a = { "MAIN" };
         processing.core.PApplet.runSketch(a, new BugZap());
     }
-<<<<<<< HEAD
-
-    public void airSeaBattle() {
-        final String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new AirSeaBattle());
-=======
-    
     public void sound1()
 	{
 		String[] a = {"MAIN"};
@@ -53,60 +46,17 @@ public class Main
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new AirSeaBattle());
->>>>>>> 3f21efb1b5a9d3d0cbafcc1d042aa89f314fd196
     }
 
-<<<<<<< HEAD
-    public void dogsCats() {
-=======
+
     public void soundMandala()
     {
         String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new SoundMandala());
     }
-    
-    public void dogsCats()
-    {
->>>>>>> b8af229bd5b2fdd3e3f403bbf3333675f3821094
-        // Polymorphism means
-        // The type is a superclass, the instance is a subclass
-
-        // This is not polymorphism
-       // final Cat topcat = new Cat("Topcat");
-
-        // This is polymorphism!
-        Animal mino = new Cat("Mino");
-        // A vitual method!
-        // Dynamic binding
-        mino.speak();
-        // This wont compile!!
-        // System.out.println(mino.getNumLives());
-        // This is an example of a cast
-        System.out.println(((Cat) mino).getNumLives());
-
-        mino = new Dog("Misty"); // This will compile!
-        // topcat = new Dog("Tara"); // This wont compile!
-
-        Animal a = new Cat("a");
-        final Animal b = new Cat("b");
-
-        System.out.println(a);
-        System.out.println(b);
-
-        a = b;
-
-        a.setName("c");
-
-        System.out.println(a);
-        System.out.println(b);
-
-        // c c will get printed out because
-        // a & b are object references
-
-    }
 
     public static void main(String[] arg){
         Main main = new Main();
-		main.sound1();        
+		main.sound2();        
     }
 }
