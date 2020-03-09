d79e87a8003c23dedee84312f7f69838e00a7d6a
StoopDJ <amirakak@gmail.com>
2020-03-09 12:20:06 +0000

    merge fixed

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -115,13 +115,10 @@ public class Main
 
     public static void main(String[] arg){
         Main main = new Main();
-<<<<<<< HEAD
 
 		main.soundSynthesis();        
-
-=======
 		main.starMap();        
->>>>>>> 10449debb2cc89acc74fc7f92757175469b0cca5
+
     }
 }
 
